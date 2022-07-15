c0e81114312d85f4ee77a00af3e73688f1943240
Kishan Raj <86339947+kishan20-00@users.noreply.github.com>
2022-07-15 11:46:38 +0530

    Updated the code in Modelling... Corrected some errors!!!

--- Spam Detector.py
+++ Spam Detector.py
@@ -1,7 +1,34 @@
 # Project to implement a spam classifier
 
-# !----First have to explore the data through EDA----!
-import pandas as pd 
+#Importing packages
+import os
+
+# Data wrangling and data visualistion 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Processing text
+import nltk
+import re
+import string
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+
+# Machine learning
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support as score
+
+# Others
+import numpy as np
+from collections import Counter
+import time
+from statistics import mean
+
+# !----First, EDA(Exploratory Data Analysis) must be done----!
 
 data = pd.read_csv("spam.csv", encoding= "latin-1") ## to read the data file
 data = data.dropna(how = "any", axis= 1)            ## to drop the null value
@@ -12,9 +39,6 @@ data.head()                                         ## to select top 5 data of t
 
 
 # let's use more visual representation of our data
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 total = len(data)                              ## get the total length of data
 plt.figure(figsize = (5,5))                    ## give the size of the plot
 plt.title("Number of spam vs ham messages")    ## give the title of the plot
@@ -35,7 +59,6 @@ plt.show()                                     ## show the plot
 data["body_len"] = data.body_text.apply(lambda x: len(x) - x.count(" "))    ## appending a new column for the new method
 
 # percentage of punctuation in the body text
-import string
 
 def count_punct(text):                                                      ## function for counting the punctuation perc(%)
     count = sum([1 for char in text if char in string.punctuation])
@@ -44,7 +67,6 @@ def count_punct(text):                                                      ## f
 data["punct%"] = data.body_text.apply(lambda x: count_punct(x))             ## appending a new column for the function
 
 # using new features(body_len) to explore the distribution
-import numpy as np
 
 bins = np.linspace(0, 200, 40)
 data.loc[data.label == 'spam', 'body_len'].plot(kind = "hist", bins =
@@ -75,25 +97,18 @@ plt.show()
 
 # !---- This is main part of the process(Cleaning Text) ----!
 
-import re
-from nltk import PorterStemmer 
-from nltk import WordNetLemmatizer 
-from nltk.corpus import stopwords
-import string
 
-# Creating a function for text cleaning
-def clean_text(text):                                               
-    text = "".join([word.lower() for word in text if word 
-            not in string.punctuation])
-    tokens = re.findall("\S+", text)
-    text = [WordNetLemmatizer().lemmatize(word) 
-                for word in tokens if word not in 
-                stopwords.words("english")]
+# Create function for text cleaning 
+def clean_text(text):
+    text = "".join([word.lower() for word in text if word not in string.punctuation])
+    tokens = re.findall('\S+', text)
+    text = [WordNetLemmatizer().lemmatize(word) for word in tokens if word not in stopwords.words("English")]
     return text
-
-# Apply function to the body_text
+# Apply function to body_text 
 data['cleaned_text'] = data['body_text'].apply(lambda x: clean_text(x))
 data[['body_text', 'cleaned_text']].head(10)
+
+
 
 
 
@@ -102,10 +117,6 @@ data[['body_text', 'cleaned_text']].head(10)
 # There are 3 methods to do vectorising but I am
 # -selecting TF- IDF vectorizer.
 # Checking the vectorizer with an example
-
-from nltk import corpus
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 corpus = ['I love bananas', 'Bananas are so amazing!', 
             'Bananas go so well with pancakes']
@@ -116,44 +127,28 @@ pd.DataFrame(corpus.toarray(), columns= tfidf_vect.get_feature_names())
 
 
 # !---- Modelling ----!
-# Train-test-split
-from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(data[['body_text', 'body_len', 'punct%']],
-data.label, random_state=42, test_size= 0.2)
-
-#Instantiate and fit TfIdf Vectorizer
+# Train-test-split Approach
+X_train, X_test, Y_train, Y_test = train_test_split(data[['body_text', 'body_len', 'punct%']], data.label, random_state = 42, test_size = 0.2)
+# Instantiate and fit TfidfVectorizer
 tfidf_vect = TfidfVectorizer(analyzer = clean_text)
-tfidf_vect_fit = tfidf_vect.fit_transform(x_train['body_text'])
-
-# Use fitted TfidfVectorizer to transform body text
-tfidf_train = tfidf_vect.transform(x_train['body_text'])
-tfidf_test = tfidf_vect.transform(x_test['body_text'])
-
-# Recombine transformed body text with body_len and Punct%
-x_train = pd.concat([x_train[['body_len', 'punct%']].reset_index(drop= True), 
-          pd.DataFrame(tfidf_train.toarray())], axis=1)
-
-x_test = pd.concat([x_test[['body_len', 'punct%']].reset_index(drop= True), 
-         pd.DataFrame(tfidf_test.toarray())], axis=1)
+tfidf_vect_fit = tfidf_vect.fit(X_train['body_text'])
+# Use fitted TfidfVectorizer to transform body text in X_train and X_test
+tfidf_train = tfidf_vect.transform(X_train['body_text'])
+tfidf_test = tfidf_vect.transform(X_test['body_text'])
+# Recombine transformed body text with body_len and punct% features
+X_train = pd.concat([X_train[['body_len', 'punct%']].reset_index(drop = True), pd.DataFrame(tfidf_train.toarray())], axis = 1)
+X_test = pd.concat([X_test[['body_len', 'punct%']].reset_index(drop = True), pd.DataFrame(tfidf_test.toarray())], axis = 1)
 
 
-# Random Forest
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import precision_recall_fscore_support
-
+# Random Forest Method
 # Instantiate randomforest Classifier
 def explore_rf_params(n_est, depth):
     rf = RandomForestClassifier(n_estimators = n_est, max_depth = depth, n_jobs = -1, random_state = 42)
-    rf_model = rf.fit(x_train, y_train)
-    y_pred = rf_model.predict(x_test)
-    
-    #precision, recall, fscore, support = rf.score(y_test, y_pred, pos_label = 'spam', average = 'binary')
-    precision= rf.score(y_test, y_pred)
-    #print(f"Est: {n_est} / Depth: {depth} ---- Precision: {round(precision, 3)} / Recall: {round(recall, 3)} / Accuracy: {round((y_pred==y_test).sum() / len(y_pred), 3)}")
-
-    
-    
+    rf_model = rf.fit(X_train, Y_train)
+    Y_pred = rf_model.predict(X_test)
+    precision, recall, fscore, support = score(Y_test, Y_pred, pos_label = 'spam', average = 'binary')
+    print(f"Est: {n_est} / Depth: {depth} ---- Precision: {round(precision, 3)} / Recall: {round(recall, 3)} / Accuracy: {round((Y_pred==Y_test).sum() / len(Y_pred), 3)}")
     
 for n_est in [50, 100, 150]:
     for depth in [10, 20, 30, None]:
@@ -164,18 +159,41 @@ for n_est in [50, 100, 150]:
 rf = RandomForestClassifier(n_estimators = 100, max_depth = None, random_state = 42, n_jobs = -1)
 # Fit model
 start = time.time()
-rf_model = rf.fit(x_train, y_train)
+rf_model = rf.fit(X_train, Y_train)
 end = time.time()
 fit_time = end - start
 # Predict 
 start = time.time()
-y_pred = rf_model.predict(x_test)
+Y_pred = rf_model.predict(X_test)
 end = time.time()
 pred_time = end - start
 # Time and prediction results
-precision, recall, fscore, support = score(y_test, y_pred, pos_label = 'spam', average = 'binary')
+precision, recall, fscore, support = score(Y_test, Y_pred, pos_label = 'spam', average = 'binary')
 print(f"Fit time: {round(fit_time, 3)} / Predict time: {round(pred_time, 3)}")
-print(f"Precision: {round(precision, 3)} / Recall: {round(recall, 3)} / Accuracy: {round((y_pred==y_test).sum() / len(y_pred), 3)}")
+print(f"Precision: {round(precision, 3)} / Recall: {round(recall, 3)} / Accuracy: {round((Y_pred==Y_test).sum() / len(Y_pred), 3)}")
+
+# Confusion matrix for RandomForestClassifier
+matrix = confusion_matrix(Y_test, Y_pred)
+sns.heatmap(matrix, annot = True, fmt = 'd')
+
+# Gradient Boosting Classifier Method
+# Instantiate GradientBoostingClassifier
+gb = GradientBoostingClassifier(random_state = 42)
+# Fit model
+start = time.time()
+gb_model = gb.fit(X_train, Y_train)
+end = time.time()
+fit_time = end - start
+# Predict 
+start = time.time()
+Y_pred = gb_model.predict(X_test)
+end = time.time()
+pred_time = end - start
+# Time and prediction results
+precision, recall, fscore, support = score(Y_test, Y_pred, pos_label = 'spam', average = 'binary')
+print(f"Fit time: {round(fit_time, 3)} / Predict time: {round(pred_time, 3)}")
+print(f"Precision: {round(precision, 3)} / Recall: {round(recall, 3)} / Accuracy: {round((Y_pred==Y_test).sum() / len(Y_pred), 3)}")
+
 
 
 # Pipeline
